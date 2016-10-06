22735623b806ed3050d6f1e62b53ba4bf55897a7
Darren Ackers <ackers86@hotmail.com>
2016-10-06 18:13:30 +0100

    updated live port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
 });
 
