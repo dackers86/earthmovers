24712f4e3a9cae85459d382dd9affbb79c15f071
Darren Ackers <ackers86@hotmail.com>
2016-10-06 21:14:02 +0100

    wippy

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(127.0.0.1:3000, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
